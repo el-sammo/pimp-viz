2449c53e1d53b551f168bd4259b1587c0735d568
Jon Barrett <jonab1974@gmail.com>
2017-08-17 11:09:03 +0000

    initial commit

---
+++ assets/controllers/HomeController.js
@@ -0,0 +1,110 @@
+(function() {
+"use strict";
+
+var app = angular.module('app');
+
+///
+// Controllers: Home
+///
+
+app.controller('HomeController', controller);
+
+controller.$inject = [
+	'$scope', '$http', '$routeParams', '$rootScope', '$location', 
+	'$modal', '$timeout', '$window',
+
+	'signupPrompter', 'deviceMgr', 'layoutMgmt',
+	'customerMgmt',
+	'messenger', 
+	'lodash',
+	// in angular, there are some angular-defined variables/functions/behaviors
+	// that are prefaced with the dollar sign ($)
+];
+
+function controller(
+	$scope, $http, $routeParams, $rootScope, $location,
+	$modal, $timeout, $window,
+	signupPrompter, deviceMgr, layoutMgmt, 
+	customerMgmt,
+	messenger, 
+	_
+) {
+	///
+	// Variable declaration
+	///
+
+	var todayDate;
+
+	///
+	// Run initialization
+	///
+
+	init();
+
+
+	///
+	// Initialization
+	///
+
+	function init() {
+		$scope.logIn = layoutMgmt.logIn;
+		$scope.signUp = layoutMgmt.signUp;
+		$scope.logOut = layoutMgmt.logOut;
+
+		$scope.account = account;
+
+
+		// For debugging
+		$scope.debugLog = debugLog;
+
+		$rootScope.$on('customerLoggedIn', onCustomerLoggedIn);
+	}
+
+
+	///
+	// Event handlers
+	///
+	
+	function onCustomerLoggedIn(evt, args) {
+		$scope.customerId = args;
+		$scope.showLogin = false;
+		$scope.showLogout = true;
+		$scope.showSignup = false;
+	}
+
+	///
+	// View methods
+	///
+
+
+	function account() {
+		if(!$scope.customerId) {
+			layoutMgmt.logIn();
+		} else {
+			$location.path('/account');
+		}
+	}
+
+	function dynamicSort(property) {
+		var sortOrder = 1;
+		if(property[0] === "-") {
+			sortOrder = -1;
+			property = property.substr(1);
+		}
+		return function (a,b) {
+			var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+			return result * sortOrder;
+		}
+	}
+
+	///
+	// Debugging methods
+	///
+	
+	function debugLog(msg) {
+		var args = Array.prototype.slice.call(arguments);
+		console.log.apply(console, args);
+	}
+}
+
+}());
